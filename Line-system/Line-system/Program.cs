667bfaa31b85d080948c631391670724d4d0de29
Laurits <laurits.mumberg@gmail.com>
2020-11-20 15:59:41 +0100

    init

---
+++ Line-system/Line-system/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Line_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
